63c835491818fd1df1b9a683e9a1fa1e816a2394
GreenFleshEater <hallo@greenflesheater.com>
2014-02-23 11:12:20 +0100

    Changed Breakpoint and Comments

--- single.php
+++ single.php
@@ -4,15 +4,15 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<div class="article">
-	
+
 	 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<?php the_content(); ?>
-	
+
 	</div>
 <?php endwhile; endif; ?>
 
 
-<?php comments_template(); ?>
+<?php comment_form(); ?>
 
 <div class="suche"><?php include ('searchform.php'); ?></div>
 
